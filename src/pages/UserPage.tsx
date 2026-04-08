ce42d0a785044e2176f39b7376c2289dd80d17c3
Everton Cruz <cruz.everton@gmail.com>
2026-04-08 18:52:51 -0300

    created service api and use on user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { api } from "../services/api"
 
 export default function UserPage () {
     const { username } = useParams()
     
-    useEffect( () => {
-      alert(username)
-    }, [username] )
+    useEffect(() => {
+      async function fetchData() {
+        const userRes = await api.get(`/users/${username}`)
+        const repoRes = await api.get(`/users/${username}/repos`)
+
+        console.log(userRes, repoRes);
+      }
+      fetchData()
+    }, [username])
+
   return(
     <div>
       <h1>GitHub Explorer Code Group User Page</h1>
