7f66e41e33e50c538627cc631b96d7edc14624d8
Everton Cruz <cruz.everton@gmail.com>
2026-04-08 18:33:39 -0300

    created username form on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function HomePage() {
+  const [username, setUsername] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate(`/user/${username}`)
+  }
+  
   return (
-    <div>
-      <h1>GitHub Explorer Code Group Home Page</h1>
+    <div className="container mt-5">
+      <h1>GitHub Explorer</h1>
+      <form onSubmit={handleSubmit} className="mt-3">
+        <input
+          className="form-control"
+          placeholder="Digite o username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button className="btn btn-primary mt-3">Buscar</button>
+      </form>
     </div>
   )
 }
