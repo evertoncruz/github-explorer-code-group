179cfa051bae07fadcb52cb773c4c6d608ef57bb
Everton Cruz <cruz.everton@gmail.com>
2026-04-08 18:38:26 -0300

    updated user page in order to show username

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 export default function UserPage () {
+    const { username } = useParams()
+    
+    useEffect( () => {
+      alert(username)
+    }, [username] )
   return(
     <div>
       <h1>GitHub Explorer Code Group User Page</h1>
