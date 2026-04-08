ab843149b386ebf542848b0a2e9a444c7721ca70
Everton Cruz <cruz.everton@gmail.com>
2026-04-08 18:26:13 -0300

    created user page base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import UserPage from './pages/UserPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/user/:username" element={<UserPage />} />
       </Routes>
     </BrowserRouter>
   )
