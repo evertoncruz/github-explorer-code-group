7425960570a104ea6f29bf5df2433bd7c58248b5
Everton Cruz <cruz.everton@gmail.com>
2026-04-08 19:13:39 -0300

    created loading state and show avatar user

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,23 +1,41 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { api } from "../services/api"
 
 export default function UserPage () {
-    const { username } = useParams()
-    
-    useEffect(() => {
-      async function fetchData() {
+  const { username } = useParams()
+  const [user, setUser] = useState(null)
+  const [repos, setRepos] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true)
         const userRes = await api.get(`/users/${username}`)
         const repoRes = await api.get(`/users/${username}/repos`)
 
-        console.log(userRes, repoRes);
+        setUser(userRes.data)
+        setRepos(repoRes.data)
+      } catch(error) {
+        console.error("Erro ao buscar o usuário", error)
+      } finally {
+        setLoading(false)
       }
-      fetchData()
-    }, [username])
+
+    }
+    fetchData()
+  }, [username])
+
+
+  if (loading) return <p>Carregando perfil...</p>;
+  if (!user) return <p>Usuário não encontrado.</p>;
 
   return(
     <div>
       <h1>GitHub Explorer Code Group User Page</h1>
+
+      <img src={user.avatar_url} alt="Avatar do usuário no github" />
     </div>
   )
 }
