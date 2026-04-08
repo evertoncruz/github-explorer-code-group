77369e153f6ef7d4df6928fbfef2f6b4074d5def
Everton Cruz <cruz.everton@gmail.com>
2026-04-08 18:30:19 -0300

    updated user and home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 export default function HomePage() {
-
   return (
-    <div className="container mt-5">
+    <div>
       <h1>GitHub Explorer Code Group Home Page</h1>
     </div>
   )
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,7 +1,7 @@
-
-
 export default function UserPage () {
   return(
-    <h1>GitHub Explorer Code Group User Page</h1>
+    <div>
+      <h1>GitHub Explorer Code Group User Page</h1>
+    </div>
   )
 }
